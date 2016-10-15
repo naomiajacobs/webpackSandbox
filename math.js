8fcffb5d3e7dbb4fed750b3a05774b11d9a09aa6
Naomi Jacobs <naomi@mavenlink.com>
2016-10-14 17:09:24 -0700

    First files

---
+++ math.js
@@ -0,0 +1,4 @@
+const add = function(a,b) { return a + b; };
+const subtract = function(a, b) { return a - b; };
+
+module.exports = { add, subtract };
---
+++ sandbox.js
@@ -0,0 +1,3 @@
+const math = require('./math');
+
+console.log(`Adding ${math.add(1,2)}`);
