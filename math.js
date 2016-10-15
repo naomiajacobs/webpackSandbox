643ef5bdba924d21c0ea64a1dbd6c81cc4b91db0
Naomi Jacobs <naomi@mavenlink.com>
2016-10-14 17:21:02 -0700

    Match math with other branch

--- math.js
+++ math.js
@@ -1,4 +1,6 @@
-const add = function(a,b) { return a + b; };
-const subtract = function(a, b) { return a - b; };
+const math = {
+  add: function(a,b) { return a + b; },
+  subtract: function(a, b) { return a - b; },
+};
 
-module.exports = { add, subtract };
+module.exports = math;
